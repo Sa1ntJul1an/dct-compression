1f9327774510c24bc0e713e64508eef1ec1d4270
Julian Reynolds <jtrman73739@gmail.com>
2024-07-05 20:05:23 -0600

    resized image

--- main.cpp
+++ main.cpp
@@ -5,10 +5,17 @@ using namespace std;
 using namespace cv;
 
 int main(){
-
+    
     string image_path = "../../engine_darkened.jpg";
     Mat image = imread(image_path, IMREAD_COLOR);
-    imshow("Test", image);
+
+    Size image_size(500, 500);
+
+    resize(image, image, image_size);
+
+    cout << image.size << endl;
+
+    imshow("Image", image);
     waitKey(0);
 
     return 0;
