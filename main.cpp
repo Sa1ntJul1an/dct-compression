f51b621f873df58852653f67b33b484b8a12d56a
Sa1ntJul1an <jtrman73739@gmail.com>
2024-08-03 14:25:34 -0600

    reading in bgr values at each index

--- main.cpp
+++ main.cpp
@@ -1,19 +1,55 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <math.h>
+#include <vector>
 
 using namespace std;
 using namespace cv;
 
+
+
+
+vector<vector<double>> get_dct_cofficients(Mat image, int blocksize){
+    int M = image.rows;
+    int N = image.cols;
+
+    double alpha_p;
+    double alpha_q;
+
+    vector<vector<double>> coefficients;
+
+    for (int p = 0; p < M; p++){
+        vector<double> column;
+        for (int q = 0; q < N; q++){
+            vector<int> index = {p, q, 0};          // bgr value at x and y index
+            cout << image.at<Vec3b>(q, p);
+        }
+    }
+
+    return coefficients;
+}
+
+
 int main(){
     
-    string image_path = "../../engine_darkened.jpg";
+    string image_path = "lenna.png";
     Mat image = imread(image_path, IMREAD_COLOR);
 
-    Size image_size(500, 500);
+    double max_rows = 1000;
+    
+    // downsize image to fit in screen
+    double scale_factor = 1.0;
+    if (image.rows > max_rows){
+        scale_factor = max_rows / image.rows;
+    }
+
+    Size image_size(image.cols * scale_factor, image.rows * scale_factor);
 
     resize(image, image, image_size);
 
     cout << image.size << endl;
+
+    vector<vector<double>> dct_coefficients = get_dct_cofficients(image, 8);
 
     imshow("Image", image);
     waitKey(0);
